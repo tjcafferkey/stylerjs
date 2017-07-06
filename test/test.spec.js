0b02d3d1a86b62ccc7b870670aace97784e39676
Tom Cafferky <tjcafferkey@gmail.com>
2017-07-06 20:36:07 +0100

    Foundations of unit testing

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,25 @@
-import "jsdom-global/register";
+"use strict";
+
+require('jsdom-global')();
 var expect = require("chai").expect;
 var assert = require("chai").assert;
+var styler = require("../lib/index");
 
-import styler from "../src/index";
+describe("getter", function() {
+    let container;
 
-describe("Styler", function() {
-	
+    before(function(){
+        container = document.createElement('div');
+        container.classList.add('test-element');
+        document.body.appendChild(container);
+    });
+
+
+    after(function() {
+        document.body.removeChild(container);
+    });
+
+    it("should expect let styles to be an object", function() {
+
+    });
 });
